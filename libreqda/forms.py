70aeeba34ac20610bca6ea7946c737ffdc57aa49
Alejandro (shadowfax) <ale.martinez.uy@gmail.com>
2013-02-07 01:57:25 -0200

    Added form: Add users to project

--- libreqda/forms.py
+++ libreqda/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from libreqda.models import Project
 
@@ -7,3 +8,9 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ('owner', 'version', 'creation_date', 'modified_date')
+
+
+class AddUserToProjectForm(forms.Form):
+    users = forms.ModelMultipleChoiceField(
+                        queryset=User.objects.all(),
+                        label='Usuarios')
