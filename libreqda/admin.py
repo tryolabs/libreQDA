28475d9cf18dd8c6474c2b6945a3ab2fbf5c751b
Alejandro <alejandro@tryolabs.com>
2013-02-13 18:08:56 -0200

    Added new models to the admin interface

--- libreqda/admin.py
+++ libreqda/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from models import Code, Document, Project
+from models import Code, Document, Project, DocumentInstance
 
 admin.site.register(Code)
-admin.site.register(Document)
 admin.site.register(Project)
+admin.site.register(Document)
+admin.site.register(DocumentInstance)
