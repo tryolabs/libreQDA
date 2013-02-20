63d585085782bf8aee8c70dedb8ee3b05845dd98
Mauro de Carvalho <mauro.dec@gmail.com>
2013-02-20 08:47:18 -0200

    Added PDF support. It has to be tweaked since some files with special characters may not be read ocrrectly.

--- libreqda/text_extraction.py
+++ libreqda/text_extraction.py
@@ -1,3 +1,12 @@
+import slate
+
+
 def txt(f):
     with open(f, 'r') as f:
         return '\r\n'.join(f.readlines())
+
+
+def pdf(f):
+    with open(f, 'r') as f:
+        pdf = slate.PDF(f)
+        return '\r\n'.join(pdf)
