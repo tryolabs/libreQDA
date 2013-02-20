b0918d145929e9c18c3e41333cd6ede95e845564
Mauro de Carvalho <mauro.dec@gmail.com>
2013-02-20 10:14:14 -0200

    Added support for .docx files.

--- libreqda/text_extraction.py
+++ libreqda/text_extraction.py
@@ -1,5 +1,7 @@
 import slate
 
+from docx import opendocx, getdocumenttext
+
 
 def txt(f):
     with open(f, 'r') as f:
@@ -10,3 +12,11 @@ def pdf(f):
     with open(f, 'r') as f:
         pdf = slate.PDF(f)
         return '\r\n'.join(pdf)
+
+
+def docx(f):
+    try:
+        doc = opendocx(f)
+        return '\r\n'.join(getdocumenttext(doc))
+    except Exception as e:
+        print e
