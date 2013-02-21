d4a1ee10e79dbd295914391486d290f39d4b7c0f
Alejandro <alejandro@tryolabs.com>
2013-02-21 15:48:42 -0200

    Added Citations model to admin

--- libreqda/admin.py
+++ libreqda/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from models import Code, Document, Project, DocumentInstance
+from models import Code, Document, Project, DocumentInstance, Citation
 
 admin.site.register(Code)
 admin.site.register(Project)
+admin.site.register(Citation)
 admin.site.register(Document)
 admin.site.register(DocumentInstance)
