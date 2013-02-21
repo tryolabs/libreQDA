1c31ac72408650ee35a0630f4908dd5be048a63f
Alejandro <alejandro@tryolabs.com>
2013-02-21 15:49:16 -0200

    Modified JsonResponse. Now it accepts all kw arguments that HttpResponse also accepts.

--- libreqda/utils.py
+++ libreqda/utils.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
 from django.utils import simplejson
+from django.http import HttpResponse
+
 
 class JsonResponse(HttpResponse):
-    def __init__(self, data):
+    def __init__(self, data, **kwargs):
         content = simplejson.dumps(
                     data,
                     indent=2,
                     ensure_ascii=False)
         super(JsonResponse, self).__init__(
                                     content=content,
-                                    mimetype='application/json; charset=utf8')
+                                    mimetype='application/json; charset=utf8',
+                                    **kwargs)
